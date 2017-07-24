b2f5eec0b531d7eee036edcca9f7ff7f14d54f3d
scott coates <scoarescoare@gmail.com>
2017-07-23 21:01:41 -0700

    updated permutation counter

--- chapters/big_o/example_12.py
+++ chapters/big_o/example_12.py
@@ -1,14 +1,20 @@
+counter= 0
 def permutation_init(str):
     permutation(str, "")
 
 
 def permutation(str, prefix):
+    global counter
+    print(f'call number: {counter}')
+    counter +=1
+
     if len(str) == 0:
         print(prefix)
     else:
         for i, s in enumerate(str):
             rem = str[0:i] + str[i + 1:]
-            permutation(rem, prefix + str[i])
+            new_prefix = prefix + str[i]
+            permutation(rem, new_prefix)
 
 
-permutation_init("abcdefg")
+permutation_init("abc")
